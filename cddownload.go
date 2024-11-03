feca0ec37018cbfd78efa19410410653350a8b2e
Andres Toro <ryujin@viewtechlabs.com>
2024-11-03 05:34:14 -0500

    collect spell data from different champion files - at

--- cddownload.go
+++ cddownload.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"example/cddownload/internal/championlist"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -15,22 +17,54 @@ func main() {
 	for _, n := range names {
 		formattedName := strings.ToLower((n.Name))
 		fileName := fmt.Sprintf("https://raw.communitydragon.org/14.21/game/data/characters/%v/%v.bin.json", formattedName, formattedName)
-		downloadFile(fmt.Sprintf("champions/%v.json", formattedName), fileName)
+		downloadFile(formattedName, fmt.Sprintf("champions/%v.json", formattedName), fileName)
+
+		time.Sleep(4 * time.Second)
 	}
 }
 
-func downloadFile(ouputFileName string, filePath string) (err error) {
-	out, err := os.Create(ouputFileName)
-	if err != nil {
-		return err
-	}
+type Abilities struct {
+	Name  string `json:"mScriptName"`
+	Spell Spell  `json:"mSpell"`
+}
 
-	defer out.Close()
+type Spell struct {
+	DataValues        json.RawMessage `json:"mDataValues"`
+	SpellCalculations json.RawMessage `json:"mSpellCalculations"`
+	CastTime          json.RawMessage `json:"mCastTime"`
+	Cooldown          json.RawMessage `json:"cooldownTime"`
+	Mana              json.RawMessage `json:"mana"`
+}
+
+func downloadFile(championName string, ouputFileName string, filePath string) (err error) {
 
 	resp, err := http.Get(filePath)
 	defer resp.Body.Close()
 
-	o, err := io.Copy(out, resp.Body)
+	ca := make(map[string]Abilities)
+	oc := make(map[string]Abilities)
+
+	b, _ := io.ReadAll(resp.Body)
+
+	e := json.Unmarshal(b, &oc)
+	_ = e
+
+	for k, info := range oc {
+		if strings.Contains(strings.ToLower(k), championName) {
+			ca[k] = info
+		}
+	}
+
+	out, err := os.Create(ouputFileName)
+	if err != nil {
+		panic(err)
+	}
+
+	defer out.Close()
+
+	as_json, _ := json.MarshalIndent(ca, "", "\t")
+
+	o, err := out.Write(as_json)
 	_ = o
 
 	fmt.Println("This is my first program in Go")
